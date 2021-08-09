74af16e288384beaceeb8b40c49bc52fe2793cc6
khurr <khurramali63@gmail.com>
2021-08-09 15:55:55 +0500

    Initial commit

---
+++ android/app/src/main/java/com/example/sendingdatatojava/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.sendingdatatojava;
+
+import androidx.annotation.NonNull;
+
+import java.util.Map;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+import io.flutter.plugin.common.MethodChannel;
+
+public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "khurramdev";
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            final Map<String, Object> arguments = call.arguments();
+
+                            if (call.method.equals("data")) {
+                                String val1 = (String) arguments.get("val");
+
+                                if (val1.equals("1")) {
+                                    result.success("datarecived");
+                                }
+                                else {
+                                    result.success("datarecived");
+                                }
+
+                        }
+                );
+    }
+}
+
+
